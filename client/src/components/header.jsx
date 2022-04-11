1ee867351b1aa578ef8188fa830c1ec0d0baee53
charutha-antares <charutha@antarx.tech>
2022-04-11 12:31:42 +0530

    Removed Fluff

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import SearchForm from './searchForm'
 import logo from './../img/logo-big.svg'
-import github from './../img/github.svg'
-import linkedin from './../img/linkedin.png'
 import i18n from './../i18n.json'
 
 export default function Header({ lang }) {
@@ -54,30 +52,6 @@ export default function Header({ lang }) {
           <div className='col-md-auto col-12 align-self-end order-1 order-md-0'>
             <SearchForm lang={lang} labels={labels} />
             <h1 className='d-none d-md-block'>{labels.inputLabel[lang]}</h1>
-          </div>
-          <div className='col-auto align-self-end my-2 d-none d-lg-block'>
-            <ul className='list-unstyled align-bottom social-list-margin'>
-              <li className='my-2'>
-                <a
-                  href='https://github.com/theDavidBarton'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-light social-link-style'>
-                  <img className='float-left social-img-style' alt='github logo' src={github} />
-                  GitHub
-                </a>
-              </li>
-              <li className='my-2'>
-                <a
-                  href='https://linkedin.com/in/theDavidBarton/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='btn btn-light float-left social-link-style'>
-                  <img className='float-left social-img-style' alt='linkedin logo' src={linkedin} />
-                  LinkedIn
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
